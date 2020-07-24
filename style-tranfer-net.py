2509149b6f19e2d71a769ee112a74dfe165f6539
hex-plex <sendhilsom7@gmail.com>
2020-07-24 06:54:28 +0530

    fixes

--- style-tranfer-net.py
+++ style-tranfer-net.py
@@ -5,13 +5,13 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg16 import VGG16
 import cv2
 
-import scipy.optimize import fmin_l_bfgs_b
+from scipy.optimize import fmin_l_bfgs_b
 
 ITERATIONS = 15
 CHANNELS = 3
 IMAGE_SIZE = 500
-IMAGE_WIDTH = 4000
-IMAGE_HEIGHT = 3000
+IMAGE_WIDTH = 500
+IMAGE_HEIGHT = 400
 STYLE_WIDTH = 500
 STYLE_HEIGHT = 400
 IMAGENET_MEAN_RGB_VALUES = [123.68, 116.779, 103.939]
@@ -20,13 +20,14 @@ STYLE_WEIGHT = 4.5
 TOTAL_VARIATION_WEIGHT = 0.995
 TOTAL_VARIATION_LOSS_FACTOR = 1.25
 
-input_image_array = cv2.imread("content.jpg")
+input_image_array = cv2.imread("content.jpg").astype(np.float64)
+input_image_array = cv2.resize(input_image_array,(500,400))
 input_image_array = np.expand_dims(input_image_array,axis=0)
 input_image_array[:,:,:,0] -= IMAGENET_MEAN_RGB_VALUES[0]
 input_image_array[:,:,:,1] -= IMAGENET_MEAN_RGB_VALUES[1]
 input_image_array[:,:,:,2] -= IMAGENET_MEAN_RGB_VALUES[2]
 
-style_image_array = cv2.imread("style.jpg")
+style_image_array = cv2.imread("style.jpg").astype(np.float64)
 style_image_array = np.expand_dims(style_image_array,axis=0)
 style_image_array[:,:,:,0] -= IMAGENET_MEAN_RGB_VALUES[0]
 style_image_array[:,:,:,1] -= IMAGENET_MEAN_RGB_VALUES[1]
@@ -34,9 +35,9 @@ style_image_array[:,:,:,2] -= IMAGENET_MEAN_RGB_VALUES[2]
 
 input_image = backend.variable(input_image_array)
 style_image = backend.variable(style_image_array)
-combination_image = backend.variable((1,IMAGE_HEIGHT, IMAGE_WIDTH, 3))
+combination_image = backend.placeholder((1,IMAGE_HEIGHT, IMAGE_WIDTH, 3))
 
-input_tensor =  backend.concatenate([input_image, style_image, combination_image])
+input_tensor =  backend.concatenate([input_image, style_image, combination_image], axis=0)
 
 model = VGG16(input_tensor=input_tensor, include_top=False)
 
